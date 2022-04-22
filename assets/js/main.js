5ca4b15082db18c90e54ee730e0d01be0e7cc380
SaadRada <saadrada742@gmail.com>
2022-04-22 14:53:59 +0000

    add dark mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,9 @@ let aside = document.querySelector("aside");
 toggler.addEventListener("click", () => {
   aside.classList.toggle("closed");
 })
+
+let themebtn = document.querySelector("#theme");
+
+themebtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+})
